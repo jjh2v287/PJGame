d3641c7843722b5afe4feb41ffda474c41fd6e9b
jjh2v2 <jjh2v2@gmail.com>
2026-03-12 22:19:41 +0900

    프로젝트 초기 파일들

---
+++ Source/PJGame/PJGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PJGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PJGame, "PJGame" );
